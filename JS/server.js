5b0a51c51bebe69d788360c19d6893fd0188c49e
Adarsh jain <adarshjain1338@gmail.com>
2022-10-22 08:59:00 -0700

    revert back to 'Secton Margin removed'

--- JS/server.js
+++ JS/server.js
@@ -8,7 +8,7 @@ const {
 
 app.set('view engine', 'ejs');
 
-mongoose.connect('mongodb+srv://major:Neepa12345@test.ubpaqfh.mongodb.net/?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://major:Neepa12345@test.ubpaqfh.mongodb.net/test');
 
 const moviesSchema = {
     title: String,
@@ -20,13 +20,12 @@ const Movie = mongoose.model('Movie', moviesSchema);
 
 app.get('/', (req, res) => {
     Movie.find({}, function (err, movies) {
-        console.log('hello');
         res.render('index', {
             moviesList: movies
         })
     })
 })
 
-app.listen(3000, function () {
+app.listen(4000, function () {
     console.log('server is running');
 })
